5b519066ae6dd85b1d72d01b29c65cc8294827a2
Adam Jakubczak <a.jakubczak@weindich.pl>
2024-07-30 21:13:24 +0200

    Added data access object

---
+++ library/db/connection.py
@@ -0,0 +1,24 @@
+import sqlite3
+import json
+
+class DataAcessObject:
+    def __init__(self):
+        
+        with open ('config.json', 'r') as file:
+            config_file = json.load(file)
+            self.db_path = config_file['db_path']
+        
+        self.connection : sqlite3.Connection
+        self.cursor : sqlite3.Cursor
+    
+    def open_connection(self):
+        
+        self.connection = sqlite3.Connection(self.db_path)
+        self.cursor = self.connection.cursor()
+        print('Connection established')
+
+    def close_connection(self):
+
+        self.cursor.close()
+        self.connection.close()
+        print('Connection terminated')
