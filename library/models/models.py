2b92a1b87bc6d8522da91383fda2723ed0253d2d
Adam Jakubczak <a.jakubczak@hotmail.com>
2024-08-04 22:17:46 +0200

    Added Book and User classes

---
+++ library/models/models.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self):
+        
+        self.book_id : int
+        self.book_title : str
+        self.book_author : str
+        self.book_isbn : int
+
+class User:
+    def __init__(self):
+
+        self.user_id : int
+        self.user_name : str
+        self.user_last_name : str
+        self.user_card_number : int
+
